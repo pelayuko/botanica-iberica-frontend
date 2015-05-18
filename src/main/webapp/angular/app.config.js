f4746d074f3ae8ab2a1ef43f397dce282198056b
pelayuko <jpelayo2008@gmail.com>
2015-05-18 16:58:36 +0200

    cambiada url de backend

--- src/main/webapp/angular/app.config.js
+++ src/main/webapp/angular/app.config.js
@@ -1,8 +1,8 @@
 // TO CFG!!
 //var serverUrl = "http://192.168.1.111:8080";
 //var serverUrl = "http://192.168.1.16:8080";
-//var serverUrl = "https://botanica-moncayo-backend.herokuapp.com";
-var serverUrl = "54.88.136.216";
+var serverUrl = "http://botanica-moncayo-backend.herokuapp.com";
+//var serverUrl = "54.88.136.216";
 
 /**
  * Configure the Routes
