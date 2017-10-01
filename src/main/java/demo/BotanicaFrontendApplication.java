0f1616d569e114fb4b84439c7b8a936a04c2aaea
Pelayo, Javier <Javier.Pelayo@adidas-group.com>
2017-10-01 22:07:48 +0200

    remove mongo logger

--- src/main/java/demo/BotanicaFrontendApplication.java
+++ src/main/java/demo/BotanicaFrontendApplication.java
@@ -2,12 +2,13 @@ package demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan
-@EnableAutoConfiguration
+@EnableAutoConfiguration(exclude={MongoAutoConfiguration.class})
 public class BotanicaFrontendApplication {
 
     public static void main(String[] args) {
